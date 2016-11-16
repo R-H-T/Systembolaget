3ea5ca29c75a7ffc8b8a1fc7639e7929ce546a10
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-16 17:27:34 +0100

    Added the initial Product-model

---
+++ Systembolaget/Product.swift
@@ -0,0 +1,25 @@
+//
+//  Product.swift
+//  Systembolaget
+//
+//  Created by Roberth Hansson-Tornéus on 16/11/2016A.
+//  Copyright © 2016 Roberth Hansson-Tornéus. All rights reserved.
+//
+
+import Foundation
+
+class Product: NSObject {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Initializers
+    
+    override init() {
+        super.init()
+        
+        // init
+    }
+
+}
