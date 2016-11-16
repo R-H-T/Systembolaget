1864c10208ede2b3c82dbd65d42ec66939953ee5
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-16 18:58:39 +0100

    Converted each of the fetched results into an object of Product

--- Systembolaget/SystembolagetClient.swift
+++ Systembolaget/SystembolagetClient.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-typealias CompletionHandler = ((_ success: Any?, _ error: NSError?)->())
+typealias CompletionHandler = ((_ success: [Product]?, _ error: NSError?)->())
 
 class SystembolagetClient: NSObject {
     
     // MARK: - Properties
     
-    var products: [Product]? = Product() // TODO: Create a model named Product?
+    var products: [Product]? = [Product]() // TODO: Create a model named Product?
     
     
     // MARK: - Methods
@@ -80,20 +80,62 @@ class SystembolagetClient: NSObject {
                     
                 } catch let error as NSError {
                     
-                    completion(data, error)
+                    completion(nil, error)
                 }
                 
                 if let products = document?.childrenNamed("artikel") {
                     
+                    var productDict: [String : Any]? = [:]
+                    var newProducts: [Product]? = [Product]()
+                    
+                    for product in products {
+                        
+                        productDict?[Product.Keys.Number] = (product as AnyObject)[Product.Keys.Number] as? String
+                        productDict?[Product.Keys.ArticleID] = (product as AnyObject)[Product.Keys.ArticleID] as? String
+                        productDict?[Product.Keys.ProductNumber] = (product as AnyObject)[Product.Keys.ProductNumber] as? String
+                        productDict?[Product.Keys.Name] = (product as AnyObject)[Product.Keys.Name] as? String
+                        productDict?[Product.Keys.Name2] = (product as AnyObject)[Product.Keys.Name2] as? String
+                        productDict?[Product.Keys.PriceVATIncluded] = (product as AnyObject)[Product.Keys.PriceVATIncluded] as? String
+                        productDict?[Product.Keys.VolumeInML] = (product as AnyObject)[Product.Keys.VolumeInML] as? String
+                        productDict?[Product.Keys.PricePerLiter] = (product as AnyObject)[Product.Keys.PricePerLiter] as? String
+                        productDict?[Product.Keys.SoldSince] = (product as AnyObject)[Product.Keys.SoldSince] as? String
+                        productDict?[Product.Keys.Expired] = (product as AnyObject)[Product.Keys.Expired] as? String
+                        productDict?[Product.Keys.ProductGroup] = (product as AnyObject)[Product.Keys.ProductGroup] as? String
+                        productDict?[Product.Keys.TypeDesc] = (product as AnyObject)[Product.Keys.TypeDesc] as? String
+                        productDict?[Product.Keys.Style] = (product as AnyObject)[Product.Keys.Style] as? String
+                        productDict?[Product.Keys.Packaging] = (product as AnyObject)[Product.Keys.Packaging] as? String
+                        productDict?[Product.Keys.Seal] = (product as AnyObject)[Product.Keys.Seal] as? String
+                        productDict?[Product.Keys.Origin] = (product as AnyObject)[Product.Keys.Origin] as? String
+                        productDict?[Product.Keys.OriginCountryName] = (product as AnyObject)[Product.Keys.OriginCountryName] as? String
+                        productDict?[Product.Keys.Manufacturer] = (product as AnyObject)[Product.Keys.Manufacturer] as? String
+                        productDict?[Product.Keys.Provider] = (product as AnyObject)[Product.Keys.Provider] as? String
+                        productDict?[Product.Keys.FromYear] = (product as AnyObject)[Product.Keys.FromYear] as? String
+                        productDict?[Product.Keys.SampledYear] = (product as AnyObject)[Product.Keys.SampledYear] as? String
+                        productDict?[Product.Keys.AlchoholLevel] = (product as AnyObject)[Product.Keys.AlchoholLevel] as? String
+                        productDict?[Product.Keys.Supply] = (product as AnyObject)[Product.Keys.Supply] as? String
+                        productDict?[Product.Keys.SupplyText] = (product as AnyObject)[Product.Keys.SupplyText] as? String
+                        productDict?[Product.Keys.Ecological] = (product as AnyObject)[Product.Keys.Ecological] as? String
+                        productDict?[Product.Keys.Koscher] = (product as AnyObject)[Product.Keys.Koscher] as? String
+                        productDict?[Product.Keys.RawIngredientsDescription] = (product as AnyObject)[Product.Keys.RawIngredientsDescription] as? String
+                        
+                        if let dictionary = productDict {
+                            
+                            let product = Product(with: dictionary)
+                            
+                            newProducts?.append(product)
+                        }
+                        
+                        productDict = [:]
+                    }
                     
                     // TODO: Add a model for representing the Product
-                    completion(products, nil)
+                    completion(newProducts, nil)
                     
                     return
                     
                 } else {
                     
-                    completion(data, NSError(domain: "SystemBolaget.client", code: 3, userInfo: ["Error" : "Error fetching products from xml document."]))
+                    completion(nil, NSError(domain: "SystemBolaget.client", code: 3, userInfo: ["Error" : "Error fetching products from xml document."]))
                 }
                 
             } else {
