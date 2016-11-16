c05370f9641362e714902584d4d2852ce8c0aa72
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-16 21:56:18 +0100

    Test run the client from the main view controller

--- Systembolaget/ViewController.swift
+++ Systembolaget/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // MARK: - Properties
+    
+    lazy var sharedClient: SystembolagetClient = {
+        
+        return SystembolagetClient.shared()
+    }()
+    
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Test Run
+        getAllProducts()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    // MARK: - Methods
+    
+    func getAllProducts(withForce flag: Bool = false) {
+        
+        if self.sharedClient.products?.count ?? 0 == 0 || flag {
+            
+            if flag {
+                // Clear any existing products before repopulating data
+                self.sharedClient.products = [Product]()
+            }
+            
+            DispatchQueue.global(qos: .background).async {
+                
+                UIApplication.shared.isNetworkActivityIndicatorVisible = true
+                
+                self.sharedClient.getProducts() {
+                    
+                    DispatchQueue.main.async {
+                        
+                        print(self.sharedClient.products?[0].number ?? "nil")
+                        
+                        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                    }
+                }
+            }
+        }
     }
-
-
 }
 
