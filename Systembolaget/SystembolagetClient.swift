cf69757c6eded16c0efbadb90cc4c3c0eb190af0
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-16 21:21:12 +0100

    Corrected the fetch method and assigned the appropriate type of the retrieved XML data

--- Systembolaget/SystembolagetClient.swift
+++ Systembolaget/SystembolagetClient.swift
@@ -14,7 +14,7 @@ class SystembolagetClient: NSObject {
     
     // MARK: - Properties
     
-    var products: [Product]? = [Product]() // TODO: Create a model named Product?
+    var products: [Product]? = [Product]()
     
     
     // MARK: - Methods
@@ -78,65 +78,63 @@ class SystembolagetClient: NSObject {
                     
                     document = try SMXMLDocument(data: data)
                     
+                    if let products = document?.childrenNamed("artikel") {
+                        
+                        var productDict: [String : Any]? = [:]
+                        var newProducts: [Product] = [Product]()
+                        
+                        for product in products as! [SMXMLElement] {
+                            
+                            productDict?[Product.Keys.Number] = "\(product.value(withPath: Product.Keys.Number) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.ArticleID] = "\(product.value(withPath: Product.Keys.ArticleID) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.ProductNumber] = "\(product.value(withPath: Product.Keys.ProductNumber) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Name] = "\(product.value(withPath: Product.Keys.Name) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Name2] = "\(product.value(withPath: Product.Keys.Name2) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.PriceVATIncluded] = "\(product.value(withPath: Product.Keys.PriceVATIncluded) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.VolumeInML] = "\(product.value(withPath: Product.Keys.VolumeInML) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.PricePerLiter] = "\(product.value(withPath: Product.Keys.PricePerLiter) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.SoldSince] = "\(product.value(withPath: Product.Keys.SoldSince) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Expired] = "\(product.value(withPath: Product.Keys.Expired) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.ProductGroup] = "\(product.value(withPath: Product.Keys.ProductGroup) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.TypeDesc] = "\(product.value(withPath: Product.Keys.TypeDesc) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Style] = "\(product.value(withPath: Product.Keys.Style) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Packaging] = "\(product.value(withPath: Product.Keys.Packaging) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Seal] = "\(product.value(withPath: Product.Keys.Seal) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Origin] = "\(product.value(withPath: Product.Keys.Origin) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.OriginCountryName] = "\(product.value(withPath: Product.Keys.OriginCountryName) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Manufacturer] = "\(product.value(withPath: Product.Keys.Manufacturer) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Provider] = "\(product.value(withPath: Product.Keys.Provider) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.FromYear] = "\(product.value(withPath: Product.Keys.FromYear) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.SampledYear] = "\(product.value(withPath: Product.Keys.SampledYear) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.AlchoholLevel] = "\(product.value(withPath: Product.Keys.AlchoholLevel) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Supply] = "\(product.value(withPath: Product.Keys.Supply) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.SupplyText] = "\(product.value(withPath: Product.Keys.SupplyText) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Ecological] = "\(product.value(withPath: Product.Keys.Ecological) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.Koscher] = "\(product.value(withPath: Product.Keys.Koscher) as? String? ?? "N/A")"
+                            productDict?[Product.Keys.RawIngredientsDescription] = "\(product.value(withPath: Product.Keys.RawIngredientsDescription) as? String? ?? "N/A")"
+                            
+                            if let dictionary = productDict {
+                                
+                                let product = Product(with: dictionary)
+                                
+                                newProducts.append(product)
+                            }
+                            
+                            productDict = [:]
+                        }
+                        
+                        completion(newProducts, nil)
+                        
+                    } else {
+                        
+                        completion(nil, NSError(domain: "SystemBolaget.client", code: 3, userInfo: ["Error" : "Error fetching products from xml document."]))
+                    }
+                    
                 } catch let error as NSError {
                     
                     completion(nil, error)
                 }
                 
-                if let products = document?.childrenNamed("artikel") {
-                    
-                    var productDict: [String : Any]? = [:]
-                    var newProducts: [Product]? = [Product]()
-                    
-                    for product in products {
-                        
-                        productDict?[Product.Keys.Number] = (product as AnyObject)[Product.Keys.Number] as? String
-                        productDict?[Product.Keys.ArticleID] = (product as AnyObject)[Product.Keys.ArticleID] as? String
-                        productDict?[Product.Keys.ProductNumber] = (product as AnyObject)[Product.Keys.ProductNumber] as? String
-                        productDict?[Product.Keys.Name] = (product as AnyObject)[Product.Keys.Name] as? String
-                        productDict?[Product.Keys.Name2] = (product as AnyObject)[Product.Keys.Name2] as? String
-                        productDict?[Product.Keys.PriceVATIncluded] = (product as AnyObject)[Product.Keys.PriceVATIncluded] as? String
-                        productDict?[Product.Keys.VolumeInML] = (product as AnyObject)[Product.Keys.VolumeInML] as? String
-                        productDict?[Product.Keys.PricePerLiter] = (product as AnyObject)[Product.Keys.PricePerLiter] as? String
-                        productDict?[Product.Keys.SoldSince] = (product as AnyObject)[Product.Keys.SoldSince] as? String
-                        productDict?[Product.Keys.Expired] = (product as AnyObject)[Product.Keys.Expired] as? String
-                        productDict?[Product.Keys.ProductGroup] = (product as AnyObject)[Product.Keys.ProductGroup] as? String
-                        productDict?[Product.Keys.TypeDesc] = (product as AnyObject)[Product.Keys.TypeDesc] as? String
-                        productDict?[Product.Keys.Style] = (product as AnyObject)[Product.Keys.Style] as? String
-                        productDict?[Product.Keys.Packaging] = (product as AnyObject)[Product.Keys.Packaging] as? String
-                        productDict?[Product.Keys.Seal] = (product as AnyObject)[Product.Keys.Seal] as? String
-                        productDict?[Product.Keys.Origin] = (product as AnyObject)[Product.Keys.Origin] as? String
-                        productDict?[Product.Keys.OriginCountryName] = (product as AnyObject)[Product.Keys.OriginCountryName] as? String
-                        productDict?[Product.Keys.Manufacturer] = (product as AnyObject)[Product.Keys.Manufacturer] as? String
-                        productDict?[Product.Keys.Provider] = (product as AnyObject)[Product.Keys.Provider] as? String
-                        productDict?[Product.Keys.FromYear] = (product as AnyObject)[Product.Keys.FromYear] as? String
-                        productDict?[Product.Keys.SampledYear] = (product as AnyObject)[Product.Keys.SampledYear] as? String
-                        productDict?[Product.Keys.AlchoholLevel] = (product as AnyObject)[Product.Keys.AlchoholLevel] as? String
-                        productDict?[Product.Keys.Supply] = (product as AnyObject)[Product.Keys.Supply] as? String
-                        productDict?[Product.Keys.SupplyText] = (product as AnyObject)[Product.Keys.SupplyText] as? String
-                        productDict?[Product.Keys.Ecological] = (product as AnyObject)[Product.Keys.Ecological] as? String
-                        productDict?[Product.Keys.Koscher] = (product as AnyObject)[Product.Keys.Koscher] as? String
-                        productDict?[Product.Keys.RawIngredientsDescription] = (product as AnyObject)[Product.Keys.RawIngredientsDescription] as? String
-                        
-                        if let dictionary = productDict {
-                            
-                            let product = Product(with: dictionary)
-                            
-                            newProducts?.append(product)
-                        }
-                        
-                        productDict = [:]
-                    }
-                    
-                    // TODO: Add a model for representing the Product
-                    completion(newProducts, nil)
-                    
-                    return
-                    
-                } else {
-                    
-                    completion(nil, NSError(domain: "SystemBolaget.client", code: 3, userInfo: ["Error" : "Error fetching products from xml document."]))
-                }
                 
             } else {
                 
