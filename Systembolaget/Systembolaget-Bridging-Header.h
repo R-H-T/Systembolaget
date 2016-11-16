bc152efc5206fe76f59ba37232dd545c6e56c25a
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-16 17:25:07 +0100

    Added manually the Objective-C bridging header and imported all the necessary headers

---
+++ Systembolaget/Systembolaget-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Systembolaget-Bridging-Header.h
+//  Systembolaget
+//
+//  Created by Roberth Hansson-Tornéus on 16/11/2016A.
+//  Copyright © 2016 Roberth Hansson-Tornéus. All rights reserved.
+//
+#import "SMXMLDocument.h"
