370c1bb9c0f1bd68568d1612398d1e1f7af03490
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-16 17:54:00 +0100

    Added Keys to Product-model

--- Systembolaget/Product.swift
+++ Systembolaget/Product.swift
@@ -11,7 +11,69 @@ import Foundation
 class Product: NSObject {
     
     // MARK: - Properties
+
     
+    struct Keys {
+        
+        static let Number = "nr"
+        static let ArticleID = "Artikelid"
+        static let ProductNumber = "Varnummer"
+        static let Name = "Namn"
+        static let Name2 = "Namn2"
+        static let PriceVATIncluded = "Prisinklmoms"
+        static let VolumeInML = "Volymiml"
+        static let PricePerLiter = "PrisPerLiter"
+        static let SoldSince = "Saljstart"
+        static let Expired = "Utgått"
+        static let ProductGroup = "Varugrupp"
+        static let `Type` = "Typ"
+        static let Style = "Stil"
+        static let Packaging = "Forpackning"
+        static let Seal = "Forslutning"
+        static let Origin = "Ursprung"
+        static let OriginCountryName = "Ursprunglandnamn"
+        static let Manufacturer = "Producent"
+        static let Provider = "Leverantor"
+        static let FromYear = "Argang"
+        static let SampledYear = "Provadargang"
+        static let AlchoholLevel = "Alkoholhalt"
+        static let Supply = "Sortiment"
+        static let SupplyText = "SortimentText"
+        static let Ecological = "Ekologisk"
+        static let Koscher = "Koscher"
+        static let RawIngredientsDescription = "RavarorBeskrivning"
+        
+        /* XML Output
+         <nr>101</nr>
+         <Artikelid>1</Artikelid>
+         <Varnummer>1</Varnummer>
+         <Namn>Renat</Namn>
+         <Namn2/>
+         <Prisinklmoms>205.00</Prisinklmoms>
+         <Volymiml>700.00</Volymiml>
+         <PrisPerLiter>292.86</PrisPerLiter>
+         <Saljstart>1993-10-01</Saljstart>
+         <Utgått>0</Utgått>
+         <Varugrupp>Okryddad sprit</Varugrupp>
+         <Typ/>
+         <Stil/>
+         <Forpackning>Flaska</Forpackning>
+         <Forslutning/>
+         <Ursprung/>
+         <Ursprunglandnamn>Sverige</Ursprunglandnamn>
+         <Producent>Pernod Ricard</Producent>
+         <Leverantor>Pernod Ricard Sweden AB</Leverantor>
+         <Argang/>
+         <Provadargang/>
+         <Alkoholhalt>37.50%</Alkoholhalt>
+         <Sortiment>FS</Sortiment>
+         <SortimentText>Ordinarie sortiment</SortimentText>
+         <Ekologisk>0</Ekologisk>
+         <Etiskt>0</Etiskt>
+         <Koscher>0</Koscher>
+         <RavarorBeskrivning>Säd.</RavarorBeskrivning>
+         */
+    }
     
     
     // MARK: - Initializers
