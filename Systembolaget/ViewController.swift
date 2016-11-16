d4bf228a01a3752da98e47c4b7963ce16b4fef3b
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-16 14:38:03 +0100

    Initial Commit

---
+++ Systembolaget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Systembolaget
+//
+//  Created by Roberth Hansson-Tornéus on 16/11/2016A.
+//  Copyright © 2016 Roberth Hansson-Tornéus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
