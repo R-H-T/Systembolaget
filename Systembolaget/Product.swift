a61bd86e1f210a874e4cc90e0ad1475dec8785fe
Roberth Hansson-Tornéus <Gawee.Narak@gmail.com>
2016-11-17 10:23:07 +0100

    Renamed `favorite` to `isFavorite` to conform to Swift's naming convention.

--- Systembolaget/Product.swift
+++ Systembolaget/Product.swift
@@ -42,7 +42,7 @@ class Product: NSObject {
     var rawIngredientsDescription: String?
     
     var lastUpdatedDate = Date()
-    var favorite: Bool = false
+    var isFavorite: Bool = false
     
     struct Keys {
         
